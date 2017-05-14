9662a2fa151d9f00069ea4e20d38e8b7eceb201c
Jeremy Gagon <jergagon1@gmail.com>
2017-05-14 11:02:50 -0700

    Add note routes and move user to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  # note resources based on notebook
   resources :notebooks do
     resources :notes
   end
-  resources :users
+  # note resources
+  resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
