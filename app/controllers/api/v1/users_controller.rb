deab0cf11dcd2a2a373d5e5a97783125c235c3c2
Jeremy Gagon <jergagon1@gmail.com>
2017-05-14 16:03:02 -0700

    implement initial auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -48,7 +48,7 @@ module Api
 
         # Only allow a trusted parameter "white list" through.
         def user_params
-          params.require(:user).permit(:username, :email)
+          params.require(:user).permit(:username, :email, :api_key)
         end
     end
   end
